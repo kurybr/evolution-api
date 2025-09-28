7d6e7616f27c41a2b78e4d1b12d4bfe4e9e9344d
Jorge Soares <jorge.soares@linaopenx.com.br>
2025-09-28 19:22:17 -0300

    fix(migration): corrigir namespace da tabela Kafka para "public"
    - Alterado o namespace da tabela Kafka de "evolution_api" para "public" para garantir a consistência no acesso aos dados.
    - Atualizados os índices e as chaves estrangeiras para refletir a nova estrutura de namespace.

--- prisma/postgresql-migrations/20250918182355_add_kafka_integration/migration.sql
+++ prisma/postgresql-migrations/20250918182355_add_kafka_integration/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "evolution_api"."Kafka" (
+CREATE TABLE "public"."Kafka" (
     "id" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
     "events" JSONB NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE "evolution_api"."Kafka" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Kafka_instanceId_key" ON "evolution_api"."Kafka"("instanceId");
+CREATE UNIQUE INDEX "Kafka_instanceId_key" ON "public"."Kafka"("instanceId");
 
 -- AddForeignKey
-ALTER TABLE "evolution_api"."Kafka" ADD CONSTRAINT "Kafka_instanceId_fkey" FOREIGN KEY ("instanceId") REFERENCES "evolution_api"."Instance"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."Kafka" ADD CONSTRAINT "Kafka_instanceId_fkey" FOREIGN KEY ("instanceId") REFERENCES "public"."Instance"("id") ON DELETE CASCADE ON UPDATE CASCADE;
